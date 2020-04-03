56169be9dc78d3e49b30ee2709342d7c3afed816
Glitch (hello-express) <none>
2020-04-03 15:37:52 +0000

    😇🙆 Checkpoint ./src/Mainpage.js:8816891/275 ./src/Mainpage.css:8816891/1112 ./src/App.css:8816891/4

--- src/Mainpage.js
+++ src/Mainpage.js
@@ -6,6 +6,10 @@ const Mainpage =()=>{
     <div id="personal-page">
       <div id="header">
       </div>
+      <div id="info">
+        <div id="avatar"><img src="https://res.cloudinary.com/techsnips/image/fetch/w_2000,f_auto,q_auto,c_fit/https://adamtheautomator.com/content/images/size/w2000/2019/10/user-1633249_1280.png"/></div>
+        <div id="bio"></div>
+      </div>
       <div className="posts">
       </div>
     </div>
