e432d15d709a72b6b33334895c7ce96145ec1c6d
Glitch (hello-express) <none>
2020-04-08 02:19:05 +0000

    💁🎢 Checkpoint ./src/Mainpage.css:8816891/60 ./server/server.js:8816891/940

--- server/server.js
+++ server/server.js
@@ -6,11 +6,8 @@ const shortid = require('shortid');
 var mongoURI = "mongodb+srv://ruslan-akhm:zuaGc0VJ@cluster0-y5h11.mongodb.net/test?retryWrites=true&w=majority"
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 var conn = mongoose.connection;
-//const ejs = require('ejs')
-const crypto = require('crypto')
+const ejs = require('ejs')
 const multer = require('multer');
-const GridFsStorage = require('multer-gridfs-storage');
-const Grid = require('gridfs-stream');
 
 const app = express();
 let gfs;
@@ -41,35 +38,7 @@ if (process.env.NODE_ENV === "production") {
   );
 }
 
-conn.once('open',() => {
-  gfs = Grid(conn.db, mongoose.mongo);
-  gfs.collection('uploads');
-})
-const storage = new GridFsStorage({
-  url: mongoURI,
-  file: (req, file) => {
-    return new Promise((resolve, reject)=>{
-      crypto.randomBytes(16, (err,buf)=>{
-        if(err) return reject(err)
-        const filename = buf.toString('hex') + path.extname(file.originalname);
-        const fileinfo={
-          filename: filename,
-          bucketName: 'uploads'
-        };
-        resolve(fileinfo);
-      })
-    })
-  }
-})
-const upload = multer({storage})
 
-app.post('/upload', upload.single('upfile'), (req,res)=>{
-  const fileObject = req.file;
-  const fName = fileObject.originalname;
-  const fType = fileObject.mimetype;
-  const fSize = fileObject.size
-  res.json({name: fName, type: fType, size: fSize})
-})
 
 // Start the listener!
 const listener = app.listen(port, () => {
