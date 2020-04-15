454fa8455513b24e6e30c7fe3ba98d2bdcfcf50b
Glitch (hello-express) <none>
2020-04-15 15:21:00 +0000

    👙🌕 Checkpoint ./src/Mainpage.js:8816891/28

--- src/Mainpage.js
+++ src/Mainpage.js
@@ -153,7 +153,7 @@ class Mainpage extends React.Component {
         <div id="modal-content">
           <form id="new-post" className="add-post-form">
             <label>Title</label>
-            <input type="text" id="post-title" />
+            <input type="text" id="post-title" maxlength="35"/>
             <label>Post</label>
             <textarea rows="18" id="post-text"></textarea>
             <input type="submit" id="submit" value="Post" />
