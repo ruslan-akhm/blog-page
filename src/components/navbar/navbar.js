6c42c224e92f7d2c9c7fd6646ad2c387cd550bfa
Glitch (hello-express) <none>
2020-10-01 15:55:43 +0000

    🍛⛷ Checkpoint ./src/components/navbar/navbar.js:8816891/261 ./src/components/navbar/navbar.css:8816891/454

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import './navbar.css';
 
 const Navbar = () => {
   return(
   <div className="navbar">
       <p>#SocialNetwork</p>
-  
+      <Link className="nav-links" to="/login">Login</Link>
+      <Link className="nav-links" to="/register">Register</Link>
       </div>
   )
 }
