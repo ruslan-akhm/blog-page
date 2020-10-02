951e47442003e65b96829fd20ac06d5c229e1f07
Glitch (hello-express) <none>
2020-10-02 15:36:25 +0000

    ⛰🙎 Checkpoint ./server/passport-config.js:8816891/846

--- server/passport-config.js
+++ server/passport-config.js
@@ -1,3 +1,25 @@
-function initialize(passsport){
-  
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+function initialize(passport){
+  const authenticateUser = (username, password, done) =>{
+    const user = getUserByUsername(username);
+    if(user == null){
+      return done(null, false, {message: "No such username found"})
+    }
+    
+    try{
+      if(await bcrypt.compare(password, user.password)){
+        return done(null, user, {message: "No such username found"})
+      }
+      else{
+        return done(null, false, {message: "Incorrect password"})
+      }
+    } catch {
+      
+    }
+  }
+  passport.use(new LocalStrategy({ usernameField: 'username' }), authenticateUser)
+  passport.serializeUser((user, done)=>{ })
+  passport.deserializeUser((_id, done)=>{ })
 }
