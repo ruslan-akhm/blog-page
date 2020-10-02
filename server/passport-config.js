a007dbcfa1e093bf45b2cc6c58762108b0fd57bf
Glitch (hello-express) <none>
2020-10-02 16:11:01 +0000

    🛥🎤 Checkpoint ./server/services/authService.js:8816891/76 ./src/components/auth/login.js:8816891/58 ./server/passport-config.js:8816891/205

--- server/passport-config.js
+++ server/passport-config.js
@@ -2,13 +2,17 @@ const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 
 function initialize(passport, getUserByUsername){
+  console.log("1) IN INITIALIZZE ")
   const authenticateUser = async (username, password, done) =>{
+    console.log("2) IN authenUSER ")
+    console.log(username)
     const user = getUserByUsername(username);
     if(user == null){
       //res.json({message: "No such username found"})
+      console.log("USER NULL")
       return done(null, false, {message: "No such username found"})
     }
-    
+    console.log("USER THERE IS ")
     try{
       if(await bcrypt.compare(password, user.password)){
         return done(null, user)
--- server/services/authService.js
+++ server/services/authService.js
@@ -7,16 +7,18 @@ const mongoose = require('mongoose');
 var mongoURI = "mongodb+srv://ruslan-akhm:zuaGc0VJ@cluster0-y5h11.mongodb.net/test?retryWrites=true&w=majority"
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 var conn = mongoose.connection;
-
 const initializePassport = require('../passport-config')
 
-initializePassport(passport, username=>{
+const init = initializePassport(passport, username=>{
   User.findOne({username: username},(err, user)=>{
     if(err)
       return err
+    consol
     return user;
   })
 })
+console.log("ININININT");
+console.log(init);
 //successRedirect to `/${username}`
 authService.post("/login", passport.authenticate('local', {
   successRedirect:'/',
