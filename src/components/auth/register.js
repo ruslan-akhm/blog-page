5d6b8b75f5b4776d0cf9f2bc66c53291a4c2ea60
Glitch (hello-express) <none>
2020-10-02 14:32:26 +0000

    🎅🏛 Checkpoint ./src/services/authService.js:8816891/333 ./src/components/auth/register.js:8816891/739

--- src/components/auth/register.js
+++ src/components/auth/register.js
@@ -6,24 +6,32 @@ import './auth.css'
 
 const Register = () => {
   
-  const register = () => {
-    
+  const [user, setUser] = useState({username:"", password:""})
+  
+  
+  const handleInput = (e) => {
+    setUser({...user,[e.target.name] : e.target.value});
+    console.log(user);
   }
   
+  const register = (e) => {
+    console.log(e)
+  }
+  //action="/api/auth/register" method="POST"
   return(
     <div>
       <Navbar />
       <div className="auth-main">
         <h1>Register</h1>
-        <form action="/api/auth/register" method="POST">  
+        <form onSubmit={(e)=>register(e)}>  
           <div className="form-box">
             <label for='username'>Username:</label>
-            <input type="text" id="username" name="username" required/>
+            <input type="text" id="username" name="username" onChange={(e)=>handleInput(e)} required/>
             <label for='password'>Password:</label>
-            <input type="text" id="password" name="password" required/>
+            <input type="text" id="password" name="password" onChange={(e)=>handleInput(e)}  required/>
             <input className="submit" type="submit" value="Register"/>
           </div>
-        </form>
+        </form >
       </div>
       <Footer />
     </div>
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,13 @@
 export default {
   register: ()=>{
-    return fetch('/api')
+    return fetch("/api/auth/register", {
+          method: "POST",
+          headers: {
+            Accept: "application/json"
+          },
+          body:""
+        })
         .then(res =>res.json())
         .then(data=>data)
-  },
+  }
 }
