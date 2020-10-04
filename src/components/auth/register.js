dd22b2633a7c717aa7eeb6db30a45a2f82ef49a1
Glitch (hello-express) <none>
2020-10-04 21:47:10 +0000

    🚣🌍 Checkpoint ./src/components/auth/register.js:8816891/4370 ./src/components/auth/auth.css:8816891/265

--- src/components/auth/register.js
+++ src/components/auth/register.js
@@ -1,57 +1,86 @@
-import React, { useState, useEffect, useContext } from 'react';
-import Navbar from '../navbar/navbar'
-import Footer from '../footer/footer'
-import authService from '../../services/authService'
-import './auth.css'
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from 'react-router-dom'
+import Navbar from "../navbar/navbar";
+import Footer from "../footer/footer";
+import authService from "../../services/authService";
+import "./auth.css";
 
 const Register = () => {
-  
-  const [user, setUser] = useState({username:"", password:""})
+  const [user, setUser] = useState({ username: "", email:"", password: "", password2:"" });
   const [message, setMessage] = useState("");
-  
+
   const resetForm = () => {
-    setUser({username:"", password:""})
-  }
-  
+    setUser({ username: "", email:"", password: "", password2:"" });
+  };
+
   const handleInput = e => {
-    setUser({...user,[e.target.name] : e.target.value});
-  }
-  
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
   const register = e => {
     e.preventDefault();
-    console.log(user)
-    authService.register(user).then(data=>{
+    console.log(user);
+    authService.register(user).then(data => {
       console.log(data);
-      setMessage(data.message)
-      if(!data.msgError){
-        setTimeout(()=>{
+      setMessage(data.message);
+      if (!data.msgError) {
+        setTimeout(() => {
           resetForm();
-        },2000)
+        }, 2000);
       }
-      
-    })
+    });
+  };
   
-  }
   //action="/api/auth/register" method="POST"
-  return(
+  return (
     <div>
       <Navbar />
       <div className="auth-main">
         <h1>Register</h1>
-        <form onSubmit={register}>  
+        <form onSubmit={register}>
           <div className="form-box">
-            <label for='username'>Username:</label>
-            <input type="text" id="username" name="username" onChange={(e)=>handleInput(e)} required/>
-            <label for='password'>Password:</label>
-            <input type="text" id="password" name="password" onChange={(e)=>handleInput(e)}  required/>
-            <input className="submit" type="submit" value="Register"/>
+            <label for="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              onChange={e => handleInput(e)}
+              required
+            />
+            <label for="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={e => handleInput(e)}
+              required
+            />
+            <label for="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              onChange={e => handleInput(e)}
+              required
+            />
+            <label for="password2">Confirm Password:</label>
+            <input
+              type="password"
+              id="password2"
+              name="password2"
+              onChange={e => handleInput(e)}
+              required
+            />
+            <input className="submit" type="submit" value="Register" />
           </div>
-        </form >
+        </form>
         <div className="message">{message}</div>
+        <p>Have an account? </p>
+        <Link className="nav-links" to="/login">Login</Link>
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
