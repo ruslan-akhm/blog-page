4ee2638b4e8c7a2881d7533f72636d47bfb1be60
Glitch (hello-express) <none>
2020-10-04 21:34:39 +0000

    🌃💄 Checkpoint ./server/passport.js:8816891/608 ./server/services/authService.js:8816891/31

--- server/services/authService.js
+++ server/services/authService.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 const authService = express.Router();
 const User = require('../models/User');
 const mongoose = require('mongoose');
-var mongoURI = "mongodb+srv://ruslan-akhm:zuaGc0VJ@cluster0-y5h11.mongodb.net/test?retryWrites=true&w=majority"
+var mongoURI = process.env.SECRET;//"mongodb+srv://ruslan-akhm:zuaGc0VJ@cluster0-y5h11.mongodb.net/test?retryWrites=true&w=majority"
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 var conn = mongoose.connection;
 //const initializePassport = require('../passport-config')
