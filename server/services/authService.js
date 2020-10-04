d634a26ec945769681aee5b4cdb41f9d74fbdccb
Glitch (hello-express) <none>
2020-10-04 22:12:10 +0000

    🙋💆 Checkpoint ./server/services/authService.js:8816891/4553

--- server/services/authService.js
+++ server/services/authService.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const passport = require('passport');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
 const authService = express.Router();
-const User = require('../models/User');
-const mongoose = require('mongoose');
-var mongoURI = process.env.SECRET;//"mongodb+srv://ruslan-akhm:zuaGc0VJ@cluster0-y5h11.mongodb.net/test?retryWrites=true&w=majority"
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+const User = require("../models/User");
+const mongoose = require("mongoose");
+var mongoURI = process.env.SECRET; //"mongodb+srv://ruslan-akhm:zuaGc0VJ@cluster0-y5h11.mongodb.net/test?retryWrites=true&w=majority"
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
 var conn = mongoose.connection;
 //const initializePassport = require('../passport-config')
 
@@ -24,38 +28,43 @@ var conn = mongoose.connection;
 console.log("ININININT");
 //console.log(init);
 //successRedirect to `/${username}`
-authService.post("/login", passport.authenticate('local', //{
-  //successRedirect:'/',
-  //failureRedirect:'/login'
-//}
-  (req,res)=>{
-  console.log("USEER REQ")
-    console.log(req.user);
-  }
-))
+authService.post(
+  "/login",
+  passport.authenticate(
+    "local", //{
+    //successRedirect:'/',
+    //failureRedirect:'/login'
+    //}
+    (req, res) => {
+      console.log("USEER REQ");
+      console.log(req.user);
+    }
+  )
+);
 
-authService.post("/register", async (req,res)=>{
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    const username = req.body.username;
-    User.findOne({username},(err,user)=>{
-      if(err)
-        res.status(500).json({message: "internal server error", error: true})
-      if(user)
-        res.status(400).json({message: "username is taken", error: true})
-      else{
-        const newUser = new User({ username: username, password: hashedPassword})
-        newUser.save(
-          err=>{
-            if(err)
-              res.status(500).json({message: "Error occured while creating account", error: true});
-            else{
-              res.status(201).json({message: "Account created!", error: false})
-            }
-          }
-        )
-      }
-    })
-    
-})
+authService.post("/register", async (req, res) => {
+  const { username, email, password, password2 } = req.body;
+
+  // const hashedPassword = await bcrypt.hash(req.body.password, 10)
+  // const username = req.body.username;
+  // User.findOne({username},(err,user)=>{
+  //   if(err)
+  //     res.status(500).json({message: "internal server error", error: true})
+  //   if(user)
+  //     res.status(400).json({message: "username is taken", error: true})
+  //   else{
+  //     const newUser = new User({ username: username, password: hashedPassword})
+  //     newUser.save(
+  //       err=>{
+  //         if(err)
+  //           res.status(500).json({message: "Error occured while creating account", error: true});
+  //         else{
+  //           res.status(201).json({message: "Account created!", error: false})
+  //         }
+  //       }
+  //     )
+  //   }
+  // })
+});
 
 module.exports = authService;
