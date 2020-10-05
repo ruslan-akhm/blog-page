2da696b4315e0c1a22644215e8c82fd195af6842
Glitch (hello-express) <none>
2020-10-05 14:30:22 +0000

    🦃🏰 Checkpoint ./package.json:8816891/2935 ./server/passportConfig.js:8816891/100

---
+++ server/passportConfig.js
@@ -0,0 +1,40 @@
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const User = require("./models/User");
+
+module.exports = function(passport) {
+  
+//   console.log("HEREREERERERERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR")
+   passport.use(
+     new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+//       //check for user
+//       console.log("LOOKING FOR USER EMAIL")
+//       User.findOne({ email }, (err, user) => {
+//         if (err) return console.log(err);
+//         if (!user) {
+//           return done(null, false, { message: "No such username found" });
+//         }
+//         console.log("LOOKING FOR PASSWORD")
+//         //check if password is correct
+//         bcrypt.compare(password, user.password, (err, isMatch) => {
+//           if (err) throw err;
+//           if (isMatch) {
+//             return done(null, user);
+//           } else {
+//             return done(null, false, { message: "Password is incorrect" });
+//           }
+//         });
+//       });
+//     })
+//   );
+//   passport.serializeUser((user, done) => {
+//     done(null, user.id);
+//   });
+
+//   passport.deserializeUser((id, done) => {
+//     User.findById(id, (err, user) => {
+//       done(err, user);
+//     });
+//   });
+};
