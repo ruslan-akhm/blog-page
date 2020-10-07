f2e601345fe969cb1c1245871e5e403f945b52ea
Glitch (hello-express) <none>
2020-10-07 01:29:49 +0000

    🌥🌅 Checkpoint ./src/context/postContext.js:8816891/1295 ./src/components/auth/login.js:8816891/9428

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from "react-router-dom";
+import { PostContext } from "../../context/postContext";
 import Navbar from "../navbar/navbar";
 import Footer from "../footer/footer";
 import authService from "../../services/authService";
 import "./auth.css";
 
 const Login = () => {
+  const { isLogged, setIsLogged } = useContext(PostContext);
   const [user, setUser] = useState({ email: "", password: "" });
   const [message, setMessage] = useState("");
+
+  let history = useHistory();
 
   const resetForm = () => {
     setUser({ email: "", password: "" });
@@ -18,11 +22,16 @@ const Login = () => {
   };
 
   const login = e => {
+    let { from } = { from: { pathname: "/register" } };
     e.preventDefault();
     console.log(user);
     authService.login(user).then(data => {
       console.log(data);
       setMessage(data.message);
+      if (data.success) {
+        setIsLogged(true);
+        history.replace(from);
+      }
       // if (!data.msgError) {
       //   setTimeout(() => {
       //     resetForm();
@@ -54,11 +63,17 @@ const Login = () => {
               onChange={e => handleInput(e)}
               required
             />
-            <input className="submit login-submit" type="submit" value="Login" />
+            <input
+              className="submit login-submit"
+              type="submit"
+              value="Login"
+            />
           </div>
         </form>
         <div className="message">{message}</div>
-        <p>Don't have an account yet? <Link to="/register">Register</Link></p>
+        <p>
+          Don't have an account yet? <Link to="/register">Register</Link>
+        </p>
       </div>
       <Footer />
     </div>
--- src/context/postContext.js
+++ src/context/postContext.js
@@ -1,18 +1,29 @@
-import React, {createContext, useState} from 'react';
+import React, { createContext, useState } from "react";
 
 export const PostContext = createContext();
 
-export const PostProvider=({ children })=>{
-  
-    const [post,setPost] = useState([]);
-    const [header, setHeader] = useState("");
-    const [avatar, setAvatar] = useState("scsdcsdcsdcd");
-  
-    return( 
-      <div>
-          <PostContext.Provider value={{post,setPost,header,setHeader,avatar,setAvatar}}>
-              { children }
-          </PostContext.Provider>
-      </div>
-    )
-}
+export const PostProvider = ({ children }) => {
+  const [post, setPost] = useState([]);
+  const [header, setHeader] = useState("");
+  const [avatar, setAvatar] = useState("scsdcsdcsdcd");
+  const [isLogged, setIsLogged] = useState(false);
+
+  return (
+    <div>
+      <PostContext.Provider
+        value={{
+          post,
+          setPost,
+          header,
+          setHeader,
+          avatar,
+          setAvatar,
+          isLogged,
+          setIsLogged
+        }}
+      >
+        {children}
+      </PostContext.Provider>
+    </div>
+  );
+};
