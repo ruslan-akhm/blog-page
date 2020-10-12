42143a7106c581691fa344245e4057d8aa0fb212
Glitch (hello-express) <none>
2020-10-12 05:38:27 +0000

    👸🐞 Checkpoint ./src/components/navbar/navbar.js:8816891/743

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import { PostContext } from "../../context/postContext";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 //import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
 import "./navbar.css";
 
@@ -8,22 +8,12 @@ const Navbar = () => {
   const { isLogged, setIsLogged, authorID, setAuthorID } = useContext(
     PostContext
   );
+ 
+  const suka = useRef();
   
   useEffect(()=>{
-    console.log("NAVBAR AUTHOR ID ")
-      
-  },[])
-  //const [myPage, setMyPage] = useState("");
-  //let history = useHistory();
-  
-  // useEffect(()=>{
-  //   setMyPage("/users/"+authorID);
-  // },[authorID])
-  
-  // const toPage = () =>{
-  //   console.log("PUSHING TO MY PAGE")
-  //   history.push("/users/c0Jtld01M")
-  // }
+    console.log(suka.current)
+  },[suka.current])
 
   return (
    
@@ -32,7 +22,7 @@ const Navbar = () => {
         #PersonalBlog
       </Link>
        {!isLogged ? null : (
-        <Link className="nav-links" to={`/users/${authorID}`}>
+        <Link className="nav-links" to={`/users/${authorID}`} ref={suka}>
           my page
         </Link>
       )} 
