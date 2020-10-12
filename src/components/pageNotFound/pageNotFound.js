73601dfa775c746671eb32d9aedb746377394e57
Glitch (hello-express) <none>
2020-10-12 18:29:50 +0000

    👼🏍 Checkpoint ./src/components/pageNotFound/pageNotFound.css:8816891/701 ./src/components/pageNotFound/pageNotFound.js:8816891/992

--- src/components/pageNotFound/pageNotFound.js
+++ src/components/pageNotFound/pageNotFound.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import "./pageNotFound.css";
 
-function PageNotFound(){
-  return(
-  <div>404 not found</div>
-  )
+function PageNotFound() {
+  return (
+    <div id="not-found">
+      <h1 style={{color:"white"}}>404 not found</h1>
+      <p>Back to <a href="/">homepage</a></p>
+    </div>
+  );
 }
 
 export default PageNotFound;
